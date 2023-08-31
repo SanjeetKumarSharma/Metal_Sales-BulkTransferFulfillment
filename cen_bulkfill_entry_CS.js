a507c936a773ee8395a6b762231a7f8c99286682
angelabCGA <91073184+angelabCGA@users.noreply.github.com>
2023-08-31 09:29:54 -0400

    Update cen_bulkfill_entry_CS.js
    Sync to central repo

--- cen_bulkfill_entry_CS.js
+++ cen_bulkfill_entry_CS.js
@@ -25,7 +25,6 @@ define(['N/currentRecord', 'N/url'],
                     console.log(title + ' | custpage_item');
 
                     //Get the parameter values from the current selections on the page
-                    //** STUB ********** */
                     var fromLocation = currRec.getValue({
                         fieldId: 'custpage_from_location_id'
                     });
@@ -35,12 +34,10 @@ define(['N/currentRecord', 'N/url'],
                     var ItemId = currRec.getValue({
                         fieldId: 'custpage_item'
                     });
-                    //alert(ItemId);
-                    var serverUrl = '/app/site/hosting/scriptlet.nl?script=3071&deploy=1&from_location=' + fromLocation + '&to_location=' + toLocation + '&item=' + ItemId;
-                    // alert(serverUrl);
-                    window.location.href = serverUrl;
+
                     //Reload the Suitelet
-                    //** STUB ********** */
+                    var serverUrl = '/app/site/hosting/scriptlet.nl?script=3071&deploy=1&from_location=' + fromLocation + '&to_location=' + toLocation + '&item=' + ItemId;
+                    window.location.href = serverUrl;
                 } catch (e) {
                     log.error('ERROR', e);
                 }
@@ -49,33 +46,35 @@ define(['N/currentRecord', 'N/url'],
             //When a user selects or deselects a checkbox, or edits a “Quantity to Fulfill” value on a selected line
             //Recalculate the “Total Quantity Selected” value and update the display
             if (context.sublistId == 'custpage_to_request_lines' && (context.fieldId == 'custpage_quantity_to_fullfill' || context.fieldId == 'custpage_checkbox')) {
-               var totalQuantity = 0.00;
+                //Commit the current line so that the field changes that triggered the recalculation are not lost
+                currRec.commitLine({sublistId: 'custpage_to_request_lines'});
+                
+                var totalQuantity = 0.00;
                 //Iterate over the line items to calculate Total Quantity Selected
                 //Total Quantity Selected = Sum of Quantity to Fulfill for all lines with the checkbox selected
-              var linecount=currRec.getLineCount({sublistId: "custpage_to_request_lines"});
-              alert(linecount);
-                 for(var i=0; i<linecount; i++){
-                   
-					 var isSelected = currRec.getSublistValue({
-                         sublistId: 'custpage_to_request_lines',
-                         fieldId: 'custpage_checkbox',
-                         line:i
-                        });
-                    if(isSelected=='true'){
-						var lineQuantityToFulfill = currRec.getSublistValue({
-                               sublistId: 'custpage_to_request_lines',
-                               fieldId: 'custpage_quantity_to_fullfill',
-                              line: i
-                             });
-                         var fullfillQuantity=lineQuantityToFulfill?lineQuantityToFulfill:0.00;
-							 totalQuantity+=fullfillQuantity;
-                    }
-					
-                 
-				}
-               
+                var linecount=currRec.getLineCount({sublistId: "custpage_to_request_lines"});
+                //alert(linecount);
+                for(var i=0; i<linecount; i++){
                 
-             currRec.setValue({fieldId: 'custpage_total_qty', value: totalQuantity});
+                    currRec.selectLine({sublistId: 'custpage_to_request_lines', line: i});
+                    var isSelected = currRec.getCurrentSublistValue({
+                        sublistId: 'custpage_to_request_lines',
+                        fieldId: 'custpage_checkbox'
+                    });
+
+                    if(isSelected){
+                        var lineQuantityToFulfill = currRec.getCurrentSublistValue({
+                            sublistId: 'custpage_to_request_lines',
+                            fieldId: 'custpage_quantity_to_fullfill'
+                        });
+                        console.log('lineQuantity', lineQuantityToFulfill);
+                        //var fullfillQuantity=lineQuantityToFulfill?lineQuantityToFulfill:0.00;
+                        totalQuantity = totalQuantity + lineQuantityToFulfill;
+                    }
+                }               
+                
+                console.log(totalQuantity);
+                currRec.setValue({fieldId: 'custpage_total_qty', value: totalQuantity});
             }
            
         }
@@ -90,57 +89,73 @@ define(['N/currentRecord', 'N/url'],
             var title = "onSubmitClick";
             alert(title);
             try {
+                //Get the request line data
+                var linesToAdd = [];
+                
                 var currRec = currentRecord.get();
-                 alert(currRec);
-                //Iterate over the lines in the Suitelet
-                for (var i = 0; i < currRec.getLineCount({
-                        sublistId: "custpage_to_request_lines"
-                    }); i++) {
-                    //Get the request line data
-                    var linesToAdd = [];
-                    //** STUB ********** */
-                  var isSelected = currRec.getSublistValue({
-                         sublistId: 'custpage_to_request_lines',
-                         fieldId: 'custpage_checkbox',
-                         line:i
-                        });
-                  var lineQuantityToFulfill = currRec.getSublistValue({
-                               sublistId: 'custpage_to_request_lines',
-                               fieldId: 'custpage_quantity_to_fullfill',
-                              line: i
-                             });
-                  var originalOpenQuantity=currRec.getSublistValue({
-                               sublistId: 'custpage_to_request_lines',
-                               fieldId: 'custpage_quantity',
-                              line: i
-                             });
-                  var transferOrderId=currRec.getSublistValue({
-                               sublistId: 'custpage_to_request_lines',
-                               fieldId: 'custpage_to_number',
-                              line: i
-                             });
-                    if(isSelected==true){
-                    //If the "Quantity to Fulfill" value is greater than the original open quantity on the line
-                    //populate the original open quantity into the Quantity field when creating the line copy.
-                    if (lineQuantityToFulfill > originalOpenQuantity) {
-                        //Populate the original open quantity into the first line
-                        linesToAdd.push({
-                            //*STUB ******************//
-                          'tranid':transferOrderId,
-                          'quantity':originalOpenQuantity
-                        });
+                var selectedItem = currRec.getValue('custpage_item');
 
-                       
-                    } else {
-                        //Only populate one line, with Quantity to Fulfill in the Quantity field
-                        //Note: UE script will handle any negative quantity deltas
-                        linesToAdd.push({
-                            //*STUB ******************//
-                          'tranid':transferOrderId,
-                          'quantity':lineQuantityToFulfill
-                        });
+                //Iterate over the lines in the Suitelet
+                for (var i = 0; i < currRec.getLineCount({sublistId: "custpage_to_request_lines"}); i++) {
+                    currRec.selectLine({sublistId: 'custpage_to_request_lines', line: i});
+
+                    var isSelected = currRec.getCurrentSublistValue({
+                        sublistId: 'custpage_to_request_lines',
+                        fieldId: 'custpage_checkbox',
+                        line:i
+                    });
+                    var lineQuantityToFulfill = currRec.getCurrentSublistValue({
+                        sublistId: 'custpage_to_request_lines',
+                        fieldId: 'custpage_quantity_to_fullfill',
+                        line: i
+                    });
+                    var originalOpenQuantity=currRec.getCurrentSublistValue({
+                        sublistId: 'custpage_to_request_lines',
+                        fieldId: 'custpage_quantity',
+                        line: i
+                    });
+                    var transferOrderId=currRec.getCurrentSublistValue({
+                        sublistId: 'custpage_to_request_lines',
+                        fieldId: 'custpage_to_id',
+                        line: i
+                    });
+                    var lineLinkKey = currRec.getCurrentSublistValue({
+                        sublistId: 'custpage_to_request_lines',
+                        fieldId: 'custpage_line_key_ref'
+                    });
+
+                    if(isSelected){
+                        //If the "Quantity to Fulfill" value is greater than the original open quantity on the line
+                        //populate the original open quantity into the Quantity field when creating the line copy.
+                        var quantityDelta = lineQuantityToFulfill - originalOpenQuantity;
+                        if(quantityDelta > 0){
+                            //Populate the original open quantity into the first line
+                            linesToAdd.push({
+                                'item': selectedItem,
+                                'quantity': originalOpenQuantity,
+                                'custcol_cen_bulkfulfill_requestto': transferOrderId,
+                                'custcol_cen_bulkfulfill_linklinekey': lineLinkKey
+                            });
+
+                            //Populate the quantity overage into an additional line. 
+                            //Do not populate the transfer order id or line unique key on the additional line.
+                            linesToAdd.push({
+                                'item': selectedItem,
+                                'quantity': quantityDelta
+                            });
+
+                        
+                        } else {
+                            //Only populate one line, with Quantity to Fulfill in the Quantity field
+                            //Note: UE script will handle any negative quantity deltas
+                            linesToAdd.push({
+                                'item': selectedItem,
+                                'quantity': lineQuantityToFulfill,
+                                'custcol_cen_bulkfulfill_requestto': transferOrderId,
+                                'custcol_cen_bulkfulfill_linklinekey': lineLinkKey
+                            });
+                        }
                     }
-                }
                 }
 
                 //Write the request line data back to the triggering Transfer Order
@@ -148,6 +163,7 @@ define(['N/currentRecord', 'N/url'],
                     var lineData = linesToAdd[lineIndex];
 
                     //Create the line and populate the values
+                    console.log(lineData);
                     //***STUB************ */
                 }
 
@@ -182,6 +198,5 @@ define(['N/currentRecord', 'N/url'],
             fieldChanged: fieldChanged,
             onCancelClick: onCancelClick,
             saveRecord: onSubmitClick
-            //sublistChanged: sublistChanged
         }
     });

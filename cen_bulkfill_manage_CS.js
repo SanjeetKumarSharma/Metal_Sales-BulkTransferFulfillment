7c28f3d4c4cb2aac1c9cc6057041e7cab85a3c06
SanjeetKumarSharma <sanjeetkumar.sharma@centricconsulting.com>
2023-09-12 18:26:12 +0530

    Lock all linked line logic implemented

--- cen_bulkfill_manage_CS.js
+++ cen_bulkfill_manage_CS.js
@@ -14,6 +14,7 @@ define(['N/record','N/url','N/ui/dialog','N/currentRecord'],
 function(record,url,dialog,currentRecord) {
     
     function pageInit(context){
+        debugger;
         var currRec = context.currentRecord;
         lockAllLinkedLines(currRec);
     }
@@ -88,12 +89,24 @@ function(record,url,dialog,currentRecord) {
         for(var i = 0; i<currRec.getLineCount({sublistId: 'item'}); i++){
             //If the Request TO, Fulfillment TO, or Line Unique Key fields are populated, lock the line
             var requestTo= currRec.getSublistValue({sublistId: 'item', fieldId: 'custcol_cen_bulkfulfill_requestto', line: i});
+            
             var fullfillmentTo=currRec.getSublistValue({sublistId: 'item', fieldId: 'custcol_cen_bulkfulfill_fulfillto', line: i});
             var lineUniqueKey=currRec.getSublistValue({sublistId: 'item', fieldId: 'custcol_cen_bulkfulfill_linklinekey', line: i});
             
             if(!isEmpty(requestTo) || !isEmpty(fullfillmentTo) || !isEmpty(lineUniqueKey)){
                 //Loop through the fields in the line and change the display type to Disabled so that no edits can be made
                 //**STUB ********//
+                
+                var ITEM_FIELD_LIST = ["amount","amounthasbeenset","backordered","billvariancestatusallbook","binitem","commitinventory","commitmentfirm","costingmethod","custcol_cen_bulkfulfill_linklinekey","custcol_cen_bulkfulfill_requestto","custcol_cen_bulkfulfill_requestto_display","custcol_oz_item_class","custcol_oz_itemtype","ddistrib","description","fulfillable","groupclosed","id","includegroupwrapper","initquantity","inventorydetailavail","isclosed","isnoninventory","isnumbered","isserial","item","item_display","itempacked","itempicked","itemtype","line","lineuniquekey","linked","linkedordbill","linkedshiprcpt","locationusesbins","noprint","oldcommitmentfirm","olditemid","onorder","printitems","quantity","quantityavailable","quantitycommitted","quantityfulfilled","quantitypacked","quantitypicked","quantityreceived","rate","sys_id","sys_parentid","unitconversionrate","units","units_display"];
+                for (var f in ITEM_FIELD_LIST) {
+        	
+                    var objField = currRec.getSublistField({
+                        sublistId: 'item',
+                        fieldId: ITEM_FIELD_LIST[f],
+                        line: i
+                    }).isDisabled = true;
+                    
+                }
             }
         }            
     }
